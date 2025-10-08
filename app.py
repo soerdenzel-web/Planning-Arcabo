9331c898f74bf11c0b52003fa8ef82ac35088b29
soerdenzel-web <soerdenzel@gmail.com>
2025-10-08 15:57:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -54,3 +54,9 @@ def download():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
